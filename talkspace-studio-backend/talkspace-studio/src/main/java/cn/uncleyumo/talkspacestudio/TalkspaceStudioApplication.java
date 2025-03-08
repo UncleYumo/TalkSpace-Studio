448a8a69552d9b7efb5cc945fceca9d63813c46f
uncle_legion <wy13921971147@163.com>
2025-03-08 19:56:35 +0800

    feat(core): 添加Maven配置和应用程序基础结构

---
+++ talkspace-studio-backend/talkspace-studio/src/main/java/cn/uncleyumo/talkspacestudio/TalkspaceStudioApplication.java
@@ -0,0 +1,13 @@
+package cn.uncleyumo.talkspacestudio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TalkspaceStudioApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TalkspaceStudioApplication.class, args);
+    }
+
+}
---
+++ talkspace-studio-backend/talkspace-studio/src/test/java/cn/uncleyumo/talkspacestudio/TalkspaceStudioApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.uncleyumo.talkspacestudio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TalkspaceStudioApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
