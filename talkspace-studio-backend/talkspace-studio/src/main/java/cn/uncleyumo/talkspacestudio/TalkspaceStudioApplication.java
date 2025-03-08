5ef7947272ac5c6eec5b90ad0cf1e3651238c752
uncle_legion <wy13921971147@163.com>
2025-03-08 20:36:30 +0800

    feat(core): 增加日志配置和测试用例

--- talkspace-studio-backend/talkspace-studio/src/main/java/cn/uncleyumo/talkspacestudio/TalkspaceStudioApplication.java
+++ talkspace-studio-backend/talkspace-studio/src/main/java/cn/uncleyumo/talkspacestudio/TalkspaceStudioApplication.java
@@ -3,6 +3,9 @@ package cn.uncleyumo.talkspacestudio;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * @author uncle_yumo
+ */
 @SpringBootApplication
 public class TalkspaceStudioApplication {
 
--- talkspace-studio-backend/talkspace-studio/src/test/java/cn/uncleyumo/talkspacestudio/TalkspaceStudioApplicationTests.java
+++ talkspace-studio-backend/talkspace-studio/src/test/java/cn/uncleyumo/talkspacestudio/TalkspaceStudioApplicationTests.java
@@ -1,13 +1,24 @@
 package cn.uncleyumo.talkspacestudio;
 
+import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
+@Slf4j
 class TalkspaceStudioApplicationTests {
 
     @Test
     void contextLoads() {
+    }
+
+    @Test
+    void testTtsGenerate() {
+        log.warn("test tts generate start");
+        System.out.println("test tts generate start");
+        log.warn("test tts generate end");
     }
 
 }
