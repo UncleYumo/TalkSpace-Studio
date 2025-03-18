6aa787a9b78127e458e86e9f19769d81623a6afc
uncle_legion <wy13921971147@163.com>
2025-03-18 23:16:38 +0800

    refactor(websocket): 移除CustomWebSocketHandler并新增WebSocketServer
    移除旧的CustomWebSocketHandler类，并新增WebSocketServer类以重构WebSocket相关功能

---
+++ talkspace-studio-backend/talkspace-studio/src/main/java/cn/uncleyumo/talkspacestudio/server/WebSocketServer.java
@@ -0,0 +1,11 @@
+package cn.uncleyumo.talkspacestudio.server;/**
+ * @author uncle_yumo
+ * @fileName WebSocketServer
+ * @createDate 2025/3/17 March
+ * @school 无锡学院
+ * @studentID 22344131
+ * @description 
+ */
+
+public class WebSocketServer {
+}
